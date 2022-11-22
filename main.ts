9509e9a5a1a5943cc3a1d5782293c66d451ae732
MrYunn <118790744+MrYunn@users.noreply.github.com>
2022-11-21 23:25:34 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,86 @@
+input.onButtonPressed(Button.A, function () {
+    servos.P0.setAngle(0)
+    basic.showLeds(`
+        . # # # .
+        . # . # .
+        . # . # .
+        . # . # .
+        . # # # .
+        `)
+    basic.pause(2000)
+    servos.P0.setAngle(10)
+    basic.showLeds(`
+        # . . . .
+        # . . . .
+        # . . . .
+        # . . . .
+        # . . . .
+        `)
+    basic.pause(2000)
+    servos.P0.setAngle(20)
+    basic.showLeds(`
+        # # # . .
+        . . # . .
+        # # # . .
+        # . . . .
+        # # # . .
+        `)
+    basic.pause(2000)
+    servos.P0.setAngle(30)
+    basic.showLeds(`
+        # # # . .
+        . . # . .
+        # # # . .
+        . . # . .
+        # # # . .
+        `)
+    basic.pause(2000)
+    servos.P0.setAngle(40)
+    basic.showLeds(`
+        # . # . .
+        # . # . .
+        # # # . .
+        . . # . .
+        . . # . .
+        `)
+    basic.pause(2000)
+    servos.P0.setAngle(50)
+    basic.showLeds(`
+        # # # . .
+        # . . . .
+        # # # . .
+        . . # . .
+        # # # . .
+        `)
+    basic.pause(2000)
+    servos.P0.setAngle(60)
+    basic.showLeds(`
+        # # # . .
+        # . . . .
+        # # # . .
+        # . # . .
+        # # # . .
+        `)
+    basic.pause(2000)
+    servos.P0.setAngle(70)
+    basic.showLeds(`
+        # # # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        . . # . .
+        `)
+    basic.pause(2000)
+    servos.P0.setAngle(80)
+    basic.showLeds(`
+        # # # . .
+        # . # . .
+        # # # . .
+        # . # . .
+        # # # . .
+        `)
+    basic.pause(2000)
+})
+input.onButtonPressed(Button.B, function () {
+	
+})
